84cbfc626c4eef5983d344a2d5da2bd1fabc3f58
Mariel125Vertz <marielvertz@gmail.com>
2026-05-20 22:47:58 -0600

    Cambios en diseño

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-os!ljmb6usufv2lp^yg^kjaet8m@tpq3ve7^@)m=pg_9sdufwp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
